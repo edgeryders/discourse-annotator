0b83759974e7ed837208086e70e7aeca00a31da9
Daniel <macbook@MBP.local>
2020-08-01 02:44:08 +0200

    #159

--- app/controllers/annotator/annotator_store/localized_tags_controller.rb
+++ app/controllers/annotator/annotator_store/localized_tags_controller.rb
@@ -2,25 +2,32 @@ class Annotator::AnnotatorStore::LocalizedTagsController < Annotator::Applicatio
 
 
   def index
+    settings = current_user.annotator_store_settings
+
     tags = AnnotatorStore::Tag.joins(:localized_tags)
                .select('annotator_store_tags.id, annotator_store_localized_tags.path localized_path')
                .order("LOWER(annotator_store_localized_tags.path) ASC")
                .where(annotator_store_localized_tags: {language_id: AnnotatorStore::UserSetting.language_for_user(current_user).id})
 
-    tags = if current_user.annotator_store_settings&.discourse_tag.present?
-             code_ids = AnnotatorStore::Annotation.select('DISTINCT tag_id')
-                            .where(topic_id: current_user.annotator_store_settings.discourse_tag.topics)
-             tags.where(id: code_ids)
-           else
-             tags.where(creator_id: current_user.id)
-           end
+    if settings&.discourse_tag.present?
+      code_ids = AnnotatorStore::Annotation.select('DISTINCT tag_id')
+                     .where(topic_id: current_user.annotator_store_settings.discourse_tag.topics)
+      tags = tags.where(id: code_ids)
+    end
 
+    if settings&.propose_codes_from_users.present?
+      user_ids = settings&.propose_codes_from_users.split.map { |n| User.find_by(username: n)&.id }.compact
+      user_ids << current_user.id
+      tags = tags.where(creator_id: user_ids)
+    elsif settings&.discourse_tag.blank?
+      tags = tags.where(creator_id: current_user.id)
+    end
 
 
     tags = tags.where("annotator_store_localized_tags.path ILIKE ?", "%#{params[:q].split.join('%')}%") if params[:q].present?
 
     respond_to do |format|
-      format.json {render json: tags.to_json(fields: %i[tag_id localized_path])}
+      format.json { render json: tags.to_json(fields: %i[tag_id localized_path]) }
     end
   end
 
@@ -34,7 +41,7 @@ class Annotator::AnnotatorStore::LocalizedTagsController < Annotator::Applicatio
     codes = codes.where("annotator_store_localized_tags.path ILIKE ?", "%#{params[:q].split.join('%')}%") if params[:q].present?
     respond_to do |format|
       format.json {
-        render json: codes.map {|c|
+        render json: codes.map { |c|
           {id: c.id, localized_path: "#{c.localized_path} (by #{c.username})"}
         }.to_json
       }
