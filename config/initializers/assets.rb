9ce9e754b0d11d23703e7dbdf9547648fbb4e800
Daniel <macbook@MBP.local>
2020-08-06 21:22:13 +0200

    Update assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,9 +1,12 @@
 Administrate::Engine.add_stylesheet('administrate')
 Rails.application.config.assets.precompile += %w(administrate.scss)
 
-# Required as otherwise assets are not precompiled.
+# Required as otherwise assets are not precompiled. damingo (Github ID), 2020-08-06
 Rails.application.config.assets.precompile += %w[administrate/application.css]
 Rails.application.config.assets.precompile += %w[administrate/application.js]
 
 Rails.application.config.assets.precompile += %w[annotator_store/application.css]
 Rails.application.config.assets.precompile += %w[annotator_store/application.js]
+
+Rails.application.config.assets.precompile += %w[administrate-field-nested_has_many/application.css]
+Rails.application.config.assets.precompile += %w[administrate-field-nested_has_many/application.js]
