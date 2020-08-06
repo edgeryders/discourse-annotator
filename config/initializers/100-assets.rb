85dff7aade292d6c415925d0934b1026a01144c4
Daniel <macbook@MBP.local>
2020-08-06 17:37:03 +0200

    Update 100-assets.rb

--- config/initializers/100-assets.rb
+++ config/initializers/100-assets.rb
@@ -1,2 +1,11 @@
 Administrate::Engine.add_stylesheet('administrate')
 Rails.application.config.assets.precompile += %w(administrate.scss)
+
+
+
+Rails.application.config.assets.paths << root.join("app", "assets", "javascripts", "annotator_store")
+Rails.application.config.assets.paths << root.join("app", "assets", "stylesheets", "annotator_store")
+
+
+Rails.application.config.assets.precompile << "application.js"
+Rails.application.config.assets.precompile << "application.css"
