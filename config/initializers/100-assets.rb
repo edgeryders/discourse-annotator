a7c0e3c4c2b70b5f67a59b29ca76077ba8b6ad9f
Daniel <macbook@MBP.local>
2020-08-06 20:34:21 +0200

    Fix assets are not precompiled

--- config/initializers/100-assets.rb
+++ config/initializers/100-assets.rb
@@ -1,2 +1,2 @@
-Administrate::Engine.add_stylesheet('administrate')
-Rails.application.config.assets.precompile += %w(administrate.scss)
+# Administrate::Engine.add_stylesheet('administrate')
+# Rails.application.config.assets.precompile += %w(administrate.scss)
