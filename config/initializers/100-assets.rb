b2e2a901dd8a8707c80d2a997238047242ab48d0
Daniel <macbook@MBP.local>
2020-08-06 17:41:07 +0200

    Update 100-assets.rb

--- config/initializers/100-assets.rb
+++ config/initializers/100-assets.rb
@@ -3,8 +3,8 @@ Rails.application.config.assets.precompile += %w(administrate.scss)
 
 
 
-Rails.application.config.assets.paths << root.join("app", "assets", "javascripts", "annotator_store")
-Rails.application.config.assets.paths << root.join("app", "assets", "stylesheets", "annotator_store")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "javascripts", "annotator_store")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets", "annotator_store")
 
 
 Rails.application.config.assets.precompile << "application.js"
