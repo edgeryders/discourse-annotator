4ab8a703a55ae9d14a0abc86197696909d1da72a
Daniel <daniel@ansorgs.de>
2023-11-26 00:01:54 +0100

    Update 20230204163902_remove_discourse_annotator_legacy_columns.rb

--- db/post_migrate/20230204163902_remove_discourse_annotator_legacy_columns.rb
+++ db/post_migrate/20230204163902_remove_discourse_annotator_legacy_columns.rb
@@ -1,7 +1,7 @@
 class RemoveDiscourseAnnotatorLegacyColumns < ActiveRecord::Migration[6.1]
   def change
-    remove_column :discourse_annotator_codes, :name_legacy
-    remove_column :discourse_annotator_user_settings, :propose_codes_from_users
-    remove_column :discourse_annotator_user_settings, :discourse_tag_id
+    remove_column :discourse_annotator_codes, :name_legacy, if_exists: true
+    remove_column :discourse_annotator_user_settings, :propose_codes_from_users, if_exists: true
+    remove_column :discourse_annotator_user_settings, :discourse_tag_id, if_exists: true
   end
 end
